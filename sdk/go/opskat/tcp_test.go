a8e5d6cb337e403ecabd719d25ac9a6303431b43
王一之 <yz@ggnb.top>
2026-04-23 14:13:46 +0800

    ✅ add tcpConn SetReadDeadline test

--- sdk/go/opskat/tcp_test.go
+++ sdk/go/opskat/tcp_test.go
@@ -4,8 +4,10 @@ package opskat
 
 import (
 	"context"
+	"errors"
 	"net"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -36,6 +38,32 @@ func TestTCPDialBasic(t *testing.T) {
 			n, err = conn.Read(buf)
 			So(err, ShouldBeNil)
 			So(string(buf[:n]), ShouldEqual, "hi")
+		})
+	})
+}
+
+func TestTCPSetDeadline(t *testing.T) {
+	Convey("SetDeadline on tcpConn", t, func() {
+		th := NewTestHost(WithMockTCP(func(addr string) (net.Conn, error) {
+			_, b := net.Pipe()
+			return b, nil
+		}))
+		defer th.Close()
+
+		conn, err := DialContext(context.Background(), "tcp", "x:9092")
+		So(err, ShouldBeNil)
+		defer conn.Close()
+
+		Convey("SetReadDeadline propagates to underlying conn", func() {
+			err := conn.SetReadDeadline(time.Now().Add(50 * time.Millisecond))
+			So(err, ShouldBeNil)
+
+			// Read should block briefly then timeout.
+			buf := make([]byte, 8)
+			_, err = conn.Read(buf)
+			So(err, ShouldNotBeNil)
+			var netErr net.Error
+			So(errors.As(err, &netErr), ShouldBeTrue)
 		})
 	})
 }
